58ad34a0ff7331e081db937fc90f5c2c63b42380
LuizRamos <luizramospe@hotmail.com>
2019-12-11 00:25:40 -0500

    lesson 6 - session tasks

--- ConcurrencyTest/ConcurrencyTest/ConfigurationPlayground.playground/Contents.swift
+++ ConcurrencyTest/ConcurrencyTest/ConfigurationPlayground.playground/Contents.swift
@@ -1,3 +1,62 @@
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+let mySharedSession = URLSession.shared
+
+mySharedSession.configuration.allowsCellularAccess
+mySharedSession.configuration.allowsCellularAccess = false
+mySharedSession.configuration.allowsCellularAccess
+
+
+let myDefaultConfiguration = URLSessionConfiguration.default
+let eConfig = URLSessionConfiguration.ephemeral
+let bConfig = URLSessionConfiguration.background(withIdentifier: "code.luizramospe.sessions")
+
+
+myDefaultConfiguration.allowsCellularAccess
+myDefaultConfiguration.allowsCellularAccess = false
+myDefaultConfiguration.allowsCellularAccess
+
+let myDefaultSession = URLSession(configuration: myDefaultConfiguration)
+myDefaultSession.configuration.allowsCellularAccess
+
+let defaultSession = URLSession(configuration: .default)
+
+/*
+We have three subclass of URLSessionTask
+ 1 - URLSessionDataTask - returns the response as an object in memory
+ 2 - URLSessionUploadTask - similar to URLSessionDataTask but it makes easier to provide a request body
+ 3 - URLSessionDownloadTask - doesn't return the response in memory but writes the data to a file
+    and returns the location of the file
+
+ */
+
+
+let configuration = URLSessionConfiguration.default
+configuration.waitsForConnectivity = true
+let session = URLSession(configuration: configuration)
+
+let urlString = "https://jsonplaceholder.typicode.com/users"
+let url = URL(string: urlString)!
+let task = session.dataTask(with: url) {
+    data, response, error in
+
+    guard let httpResponse = response as? HTTPURLResponse,
+    httpResponse.statusCode == 200 else {
+        return
+    }
+
+    guard let data = data else {
+        print(error.debugDescription)
+        return
+    }
+
+    if let result = String(data: data, encoding: .utf8) {
+        print(result)
+    }
+
+    PlaygroundPage.current.finishExecution() //Used to finish the automatic execution of playground
+
+}
+task.resume()
+
